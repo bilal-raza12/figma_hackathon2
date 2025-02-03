8137f770ab0df3039d41055d4de58918170cadc4
bilal-raza12 <160725888+bilal-raza12@users.noreply.github.com>
2025-02-03 21:24:47 +0500

    Milestone 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import Hero from "@/components/hero";
 import Banner from "@/components/banner";
 import NewArrivals from "@/components/newarrivals";
-import TopSelling from "@/components/topselling";
+
 import Dress from "@/components/dress";
-import Reviews from "@/components/reviews";
+
 
 export default function Home() {
   return (
